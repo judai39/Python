5245a3a05f74f856aec927f81d33179fba1500e2
judai39 <dhy2522609443@163.com>
2025-12-27 15:39:39 +0800

    键盘操作

--- selenium/1.7.2 键盘操作.py
+++ selenium/1.7.2 键盘操作.py
@@ -7,7 +7,7 @@ import time
 
 driver=webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get("https://www.bilibili.com")
-element_input=driver.find_element(By.XPATH,"//div[@class='bili-header large-header']//input[@class]='nav-search-input'")
+element_input=driver.find_element(By.XPATH,"//div[@class='bili-header large-header']//input[@class='nav-search-input']")
 element_input.send_keys("hello world")
 time.sleep(2)
 element_input.send_keys(Keys.CONTROL,'a')
@@ -15,3 +15,4 @@ time.sleep(2)
 element_input.send_keys(Keys.CONTROL,'x')
 time.sleep(2)
 element_input.send_keys(Keys.CONTROL,'v')
+time.sleep(10)
