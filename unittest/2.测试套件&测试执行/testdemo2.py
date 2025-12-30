40779625f032c0d55e5a005cd31f9960458e37a8
judai39 <dhy2522609443@163.com>
2025-12-30 11:01:04 +0800

    suite识别测试的方法前面需要带有-test

--- unittest/2.测试套件&测试执行/testdemo2.py
+++ unittest/2.测试套件&测试执行/testdemo2.py
@@ -1,5 +1,6 @@
 import unittest
 import unittest.test
 class TestDemo2(unittest.TestCase):
-    def test_def(self):
+    # 该方法不会被suite套件识别,因为开头没有test
+    def standar_test_def(self):
         print("这是测试demo2中的测试方法")
