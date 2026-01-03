7c6982a65766752307361320d6a1b1e65db9bb34
judai39 <dhy2522609443@163.com>
2026-01-03 11:18:21 +0800

    fixture装饰器参数-autouse

---
+++ pytest/2.2_fixture_params/autouse/fixture_autouse_sample.py
@@ -0,0 +1,16 @@
+import pytest
+
+# autouse参数(默认false)设置为true,可被观察到的所有测试方法都会自动调用该装饰器
+
+@pytest.fixture(autouse='true')
+def autouse_fixture():
+    print("我是autouse修饰的装饰器,我会被自动调用,无需传入参数")
+
+class TestAutouseFixture:
+    def test_autouse_fixture1(self):
+        print("测试方法001")
+    def test_autouse_fixture2(self):
+        print("测试方法002")
+
+if __name__=="__main__":
+    pytest.main(["-vs","pytest/2.2_fixture_params/autouse/fixture_autouse_sample.py"])
