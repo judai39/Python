c135d3dfe63d81f878cd31ad2b086c2974aba6c8
judai39 <dhy2522609443@163.com>
2026-01-04 09:54:37 +0800

    fixture装饰器参数-name

---
+++ pytest/2.2_fixture_params/name/fixture_name_sample.py
@@ -0,0 +1,12 @@
+import pytest
+@pytest.fixture(name="new_fixture_name")
+def old_fixture_name():
+    print("这里是装饰器")
+
+def test_001(new_fixture_name):
+    print("将装饰器以新的名字传入测试方法,生效")
+def test_002(old_fixture_name):
+    print("将装饰器以旧的名字传入测试方法,不生效")
+
+if __name__=="__main__":
+    pytest.main(['-vs','pytest/2.2_fixture_params/name/fixture_name_sample.py'])
