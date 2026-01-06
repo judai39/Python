e17766f13df80b7ebb7380d05458c4fac2308dbf
judai39 <dhy2522609443@163.com>
2026-01-06 15:16:29 +0800

    xfail关于测试结果的返回情况不同

--- pytest/4_mark/mark_xfail/mark_xfail_sample.py
+++ pytest/4_mark/mark_xfail/mark_xfail_sample.py
@@ -1,8 +1,15 @@
 import pytest
-
+# xfail测试成功返回xpass（常用于提示未修复的已知bug）
 @pytest.mark.xfail(2>1,reason="标注为预期失败")
 def test_001():
-    print("----预期失败的测试方法----")
+    print("----xpass尚未修复bug----")
+    assert True
+
+# xfail测试失败返回xfail（常用于提示未实现的功能）
+@pytest.mark.xfail(2>1,reason="标注为预期失败")
+def test_002():
+    print("----xfail尚未实现功能----")
+    assert False
 
 if __name__=="__main__":
     pytest.main(["-vs","pytest/4_mark/mark_xfail/mark_xfail_sample.py"])
